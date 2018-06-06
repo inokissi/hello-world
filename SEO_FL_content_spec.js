4b1beaf5c40c0753a792b0ec4b2f9718a7f7b031
Penny <pennygkogka@gmail.com>
2018-06-06 16:33:29 +0300

    Add files via upload

--- SEO_FL_content_spec.js
+++ SEO_FL_content_spec.js
@@ -4,39 +4,69 @@
 // Project : PCM
 // Sprint : 11
 //
-describe('Freelancer Listing : Content', function(){
+describe('Freelancer Listing : SEO page optimisation', function(){
   var url,Category,H1,H2,Meta
 
   //it('should check if the Category is selected when searching by a term that matches a Category', function(){
   //})
 
-  it('should have specific content ', function(){
-    //cy.viewport('iphone-6')
+  // it('Title', function(){
+  //   cy.readFile('cypress/integration/SEO_FL_content.json').then(function (json) {
+  //    for (var i = 0; i < 3; i++) {
+  //
+  //      // Visit website
+  //      cy.visit(json[i].url)
+  //    }
+  //   })
+  // })
+
+  it('H1 Title ', function(){
+
     cy.readFile('cypress/integration/SEO_FL_content.json').then(function (json) {
-     for (var i = 0; i < 3; i++) {
+     for (var i = 0; i < json.length; i++) {
 
        // Visit website
        cy.visit(json[i].url)
 
-       // Check the Title Tag
-       // cy.title().should('eq', json[i].title_tag)
-
        // Check H1 Title
        cy.get('.heading__featured-seo-excerpt').should('contain',json[i].H1)
-
-       // Check H2 Title
-       cy.get('.featured-seo__title').should('contain',json[i].H2)
-
-       // Check Content
-       cy.get('.featured-seo__content').should('contain',json[i].content)
-
-       // Check Meta Description
-       //cy.get('[content]').should('contain',json[i].meta_description)
-
-       // Check that pagination is working
-       cy.get('#freelancer-listing-pager > :nth-child(2) > a').click()
-       cy.get('#main-container')
      }
     })
   })
+  it('H2 Title', function(){
+    cy.readFile('cypress/integration/SEO_FL_content.json').then(function (json) {
+     for (var i = 0; i < json.length; i++) {
+
+       // Visit website
+       cy.visit(json[i].url)
+
+       // Check H2 Title
+       cy.get('.featured-seo__title').should('contain',json[i].H2)
+     }
+    })
+  })
+  it('Content', function(){
+    cy.readFile('cypress/integration/SEO_FL_content.json').then(function (json) {
+     for (var i = 0; i < json.length; i++) {
+
+       // Visit website
+       cy.visit(json[i].url)
+
+       // Check Content
+       cy.get('.featured-seo__content').should('contain',json[i].content)
+     }
+    })
+  })
+  // it('Meta Description', function(){
+  //   cy.readFile('cypress/integration/SEO_FL_content.json').then(function (json) {
+  //    for (var i = 0; i < 3; i++) {
+  //
+  //      // Visit website
+  //      cy.visit(json[i].url)
+  //
+  //      // Check Meta Description
+  //      //cy.get('[content]').should('contain',json[i].meta_description)
+  //    }
+  //   })
+  // })
 })
