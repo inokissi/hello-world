03b853ed06d4a001f1fc141f9c19dbb12a4f4ae0
Penny <pennygkogka@gmail.com>
2018-06-20 17:26:39 +0300

    Add files via upload

--- PayInvoice_spec.js
+++ PayInvoice_spec.js
@@ -1,16 +1,21 @@
 describe('Pay Invoice', function() {
-  it('should login', function() {
+  it('should go to Workstream and release funds', function() {
+
+    // Login
     cy.visit('/site/login')
     cy.get('#reactContainer .login-form-container form input[name="LoginForm[email]"]').type('penny+1438948203@peopleperhour.com')
     cy.get('#reactContainer .login-form-container form input[name="LoginForm[password]"]').type('qwerty')
     cy.get('#reactContainer .login-form-container form input[type="submit"]').click()
     cy.wait(3000)
+
+    // Go to Workstream
     cy.visit('stream/view?id=12942256')
-  })
-  it('should go to Workstream', function() {
+
+    // Click on the "Release" button
     cy.get('#reactContainer .invoice-release-btn').contains('Release').should('have.class','btn').click()
-  })
-  it('should click on the "Release funds" button', function() {
+
+    // click on the "Release funds" button'
     cy.get('.modal-footer').contains('Release Funds').should('have.class','btn').click()
+    //cy.get('[data-bb-handler="confirm"]').click()
   })
 })
