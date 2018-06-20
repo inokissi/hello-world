ff030376c15800cabc9d257c07be66f089cf36d2
Penny <pennygkogka@gmail.com>
2018-06-20 17:25:26 +0300

    Add files via upload

--- Raise_Invoice_spec.js
+++ Raise_Invoice_spec.js
@@ -1,23 +1,29 @@
 describe('Raise Invoice', function() {
-  it('login', function() {
+  it('should go to Workstream and raise an invoice', function() {
+
+    // Login
     cy.visit('/site/login')
     cy.get('#reactContainer .login-form-container form input[name="LoginForm[email]"]').type('inokissi@yahoo.gr')
     cy.get('#reactContainer .login-form-container form input[name="LoginForm[password]"]').type('qwerty123')
     cy.get('#reactContainer .login-form-container form input[type="submit"]').click()
     cy.wait(3000)
+
+    // Go to Workstream
     cy.visit('stream/view?id=12942256')
+
+    // Click on "Raise Invoice" tab in order to open the Invoice form
     cy.get('#stream-message-box .text').contains('Raise Invoice').click()
-  })
-  it('enter description', function() {
+
+    // Enter description
     cy.get('#stream-message-box .stream-attachment-form input[id="description"]').type('Description entered')
-  })
-  it('enter amount', function() {
+
+    // Enter price
     cy.get('#stream-message-box .stream-attachment-form input[id="cost"]').type('10')
-  })
-  it('check the checkbox', function() {
+
+    // Check the aknowledgement checkbox
     cy.get('#jobAcknowledge').check({ force: true })
-  })
-  it('click the SEND button', function() {
+
+    // Click the "Send" button
     cy.get('#stream-message-box .post-controls').contains('Send').click()
-  })
+})
 })
