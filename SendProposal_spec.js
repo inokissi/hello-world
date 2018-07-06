29376eb45c86ddcfcdc9edfc0c8e2eead385a001
Penny <pennygkogka@gmail.com>
2018-07-06 18:48:30 +0300

    Add files via upload

---
+++ SendProposal_spec.js
@@ -0,0 +1,22 @@
+describe('Send Proposal', function(){
+  it('The user should loggin successfully', function(){
+  	cy.clearCookies()
+	cy.LoginAsSeller()
+    cy.wait(3000)
+
+    // Go to Freelancer listing
+    cy.visit('/freelance-jobs/')
+
+    // Select the first job of the list
+    cy.get('#job-listing-listview .btn').contains('Send Proposal').click()
+  	
+  	// Enter description
+    cy.get('#stream-message-box .description').should('be.visible').type('This is my proposal!')
+
+  	// Enter amount
+    cy.get('#stream-message-box .total ').type('30')
+
+    // Click the "Send" button to submit form
+    cy.get('#sendSimple').contains('Send').click()
+  })
+})
